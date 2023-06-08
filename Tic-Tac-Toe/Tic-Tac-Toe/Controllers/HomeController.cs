4bf827367b40632241dab4f903263f2440ec78f2
Mitchell Snoek | Technoberg IT Services <msnoek@technoberg.nl>
2023-06-08 16:16:56 +0200

    commit

--- Tic-Tac-Toe/Tic-Tac-Toe/Controllers/HomeController.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Controllers/HomeController.cs
@@ -12,10 +12,110 @@ namespace Tic_Tac_Toe.Controllers
         {
             _logger = logger;
         }
+        private void MakeButtons()
+        {
+            var GameModel = Game.GetInstance();   
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+            GameModel.Buttons.Add(new Button());
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var GameModel = Game.GetInstance(); 
+            if (!GameModel.Buttons.Any())
+            {
+                MakeButtons();
+            }
+
+            return View(GameModel);
+        }
+
+
+        public IActionResult Button1Pressed()
+        {
+            int buttonpressed = 1;
+            return ButtonPressed(buttonpressed);
+        }
+
+        public IActionResult Button2Pressed()
+        {
+            int buttonpressed = 2;
+            return ButtonPressed(buttonpressed);
+        }
+
+        public IActionResult Button3Pressed()
+        {
+            int buttonpressed = 3;
+            return ButtonPressed(buttonpressed);
+        }
+
+        public IActionResult Button4Pressed()
+        {
+            int buttonpressed = 4;
+            return ButtonPressed(buttonpressed);
+        }
+
+        public IActionResult Button5Pressed()
+        {
+            int buttonpressed = 5;
+            return ButtonPressed(buttonpressed);
+        }
+
+        public IActionResult Button6Pressed()
+        {
+            int buttonpressed = 6;
+            return ButtonPressed(buttonpressed);
+        }
+
+        public IActionResult Button7Pressed()
+        {
+            int buttonpressed = 7;
+            return ButtonPressed(buttonpressed);
+        }
+        public IActionResult Button8Pressed()
+        {
+            int buttonpressed = 8;
+            return ButtonPressed(buttonpressed);
+        }
+        public IActionResult Button9Pressed()
+        {
+            int buttonpressed = 9;
+            return ButtonPressed(buttonpressed);
+        }
+
+        public IActionResult ButtonPressed(int buttonpressed)
+        {
+            var GameModel = Game.GetInstance();
+            int currentplayer = GameModel.CurrentPlayer;
+            string val = "X";
+            if (currentplayer % 2 == 0)
+            {
+                val = "X";
+            }
+            else if (currentplayer % 2 == 1)
+            {
+                val ="O";
+            }
+            var ButtonModel = new Button();
+            ButtonModel.ButtonValue = val;
+
+            GameModel.Buttons[buttonpressed - 1] = ButtonModel;
+
+            GameModel.CurrentPlayer += 1;
+            return RedirectToAction(nameof(Index));
+            //return CheckWin();
+        }
+
+        public IActionResult CheckWin()
+        {
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
--- Tic-Tac-Toe/Tic-Tac-Toe/Models/IndexModel.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Models/IndexModel.cs
@@ -2,14 +2,38 @@ namespace Tic_Tac_Toe.Models
 {
     public class Game
     {
+        private static Game _game = null;
+        public static Game GetInstance(bool isNew = false)
+        {
+            if (_game == null || isNew)
+            {
+                _game = new Game();
+            }
+
+            return _game;
+        }
+
+
+
         public int CurrentPlayer { get; set; }
         public int ScorePlayerX { get; set; }
         public int ScorePlayerO { get; set; }
         public List<Button> Buttons { get; set; }
 
+
+        public Game()
+        {
+            Buttons = new List<Button>();
+        }
+
     }
     public class Button
     {
         public string ButtonValue { get; set; }
+
+        public Button()
+        {
+            ButtonValue = " ";
+        }
     }
 }
